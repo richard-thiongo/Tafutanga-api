b4a52c32faf2cc537855cac1963143565beaab0c
richard-thiongo <thiongorichard0@gmail.com>
2026-06-04 21:53:09 +0300

    updated db connection

--- src/config/database.js
+++ src/config/database.js
@@ -2,15 +2,20 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
 });
 
 pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
   process.exit(-1);
+});
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring database client:', err.stack);
+  }
+  console.log('Successfully connected to the PostgreSQL database!');
+  release();
 });
 
 module.exports = {
